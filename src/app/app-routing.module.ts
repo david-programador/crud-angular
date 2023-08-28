b50ece912f346b520d23d4996b433ce93497793f
DTORRES\d_tor <d_torres@outloock.com.br>
2023-08-28 13:36:27 -0300

    crud-completo obs:. falta ajustar CSS

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,16 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { EditCourseComponent } from './edit-course/edit-course.component';
 
 const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: 'courses' },
   {
     path: 'courses',
     loadChildren: () => import('./courses/courses.module').then(m => m.CoursesModule)
+  },
+  {
+    path: "course/update/:id",
+    component: EditCourseComponent
   }
 ];
 
--- src/app/edit-course/edit-course.component.ts
+++ src/app/edit-course/edit-course.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { CoursesService } from '../courses/services/courses.service';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Route, Router } from '@angular/router';
 import { Course } from '../courses/model/course';
 
 @Component({
@@ -13,15 +13,47 @@ export class EditCourseComponent implements OnInit {
   id!: string;
   course!: Course;
 
+
   constructor(private coursesService: CoursesService,
-    private activatedRoute: ActivatedRoute) { }
+    private router: Router,
+    private activatedRoute: ActivatedRoute) {
+
+     }
 
   ngOnInit(): void {
     // Recuperar o course pelo ID
-    this.id = this.activatedRoute.snapshot.url[1].path;
+    this.id = this.activatedRoute.snapshot.url[2].path;
 
-    this.coursesService.getCoursesById(this.id).subscribe((course: Course) => {
+    this.coursesService.chamarPeloCoursesById(this.id).subscribe((course: Course) => {
       this.course = course;
-    })
+
+
+    });
   }
+
+  updateCourse(): void {}
+
+  cancel(): void {
+    this.router.navigate(['/courses/'])
+  }
+selecionarCourse(course: Course){
+  this.coursesService.chamarPeloCoursesById(course._id)
+  .subscribe(resposta => {
+
+  });
+}
+salvarCurso() {
+  const cursoAdiciona: Course = {
+    _id: this.id,
+    name: this.course.name,
+    category: this.course.category
+  }
+  console.log(cursoAdiciona);
+
+  this.coursesService.atualizarCourse(cursoAdiciona).subscribe(response => {
+    console.log(response)
+  })
+
+}
+
 }
